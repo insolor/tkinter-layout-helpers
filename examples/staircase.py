7df35817cadaa4bf961319c4637de8aac97fc760
insolor <insolor@gmail.com>
2022-02-11 13:25:18 +0300

    Minimal implementation, add an example

---
+++ examples/staircase.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+
+from tk_grid_helper import grid_manager
+
+root = tk.Tk()
+
+with grid_manager(root, sticky=tk.EW) as grid:
+    grid.new_row() \
+        .add(tk.Label(text="0", width=20)) \
+        .add(tk.Label(text="1", width=20)) \
+        .add(tk.Label(text="2", width=20)) \
+        .add(tk.Label(text="3", width=20)) \
+        .add(tk.Label(text="4", width=20))
+    
+    grid.new_row().add(tk.Entry()).column_span(4).add(tk.Entry()).column_span(1)
+    grid.new_row().add(tk.Entry()).column_span(3).add(tk.Entry()).column_span(2)
+    grid.new_row().add(tk.Entry()).column_span(2).add(tk.Entry()).column_span(3)
+    grid.new_row().add(tk.Entry()).column_span(1).add(tk.Entry()).column_span(4)
+    
+    grid.columnconfigure(0, weight=1)
+    grid.columnconfigure(1, weight=1)
+    grid.columnconfigure(2, weight=1)
+    grid.columnconfigure(3, weight=1)
+    grid.columnconfigure(4, weight=1)
+
+# The code above gives the same result as the code below:
+# tk.Label(root, text=0, width=20).grid(row=0, column=0, sticky=tk.EW)
+# tk.Label(root, text=1, width=20).grid(row=0, column=1, sticky=tk.EW)
+# tk.Label(root, text=2, width=20).grid(row=0, column=2, sticky=tk.EW)
+# tk.Label(root, text=3, width=20).grid(row=0, column=3, sticky=tk.EW)
+# tk.Label(root, text=4, width=20).grid(row=0, column=4, sticky=tk.EW)
+
+# tk.Entry(root).grid(column=0, row=1, columnspan=4, sticky=tk.EW)
+# tk.Entry(root).grid(column=4, row=1, columnspan=1, sticky=tk.EW)
+
+# tk.Entry(root).grid(column=0, row=2, columnspan=3, sticky=tk.EW)
+# tk.Entry(root).grid(column=3, row=2, columnspan=2, sticky=tk.EW)
+
+# tk.Entry(root).grid(column=0, row=3, columnspan=2, sticky=tk.EW)
+# tk.Entry(root).grid(column=2, row=3, columnspan=3, sticky=tk.EW)
+
+# tk.Entry(root).grid(column=0, row=4, columnspan=1, sticky=tk.EW)
+# tk.Entry(root).grid(column=1, row=4, columnspan=4, sticky=tk.EW)
+
+# root.grid_columnconfigure(0, weight=1)
+# root.grid_columnconfigure(1, weight=1)
+# root.grid_columnconfigure(2, weight=1)
+# root.grid_columnconfigure(3, weight=1)
+# root.grid_columnconfigure(4, weight=1)
+
+root.mainloop()
--- tk_grid_helper/tk_grid_helper.py
+++ tk_grid_helper/tk_grid_helper.py
@@ -1,21 +1,85 @@
 
 import contextlib
 import tkinter as tk
+from typing import List
 
 
 class Cell:
-    def __init__(self):
-        pass
+    def __init__(self, widget: tk.Widget, column_index: int, row_index: int,
+            **kwargs):
+        self.widget = widget
+        self.column_index = column_index
+        self.row_index = row_index
+        self.column_span = 1
+        self.row_span = 1
+        self.kwargs = kwargs
 
 
 class Row:
-    def __init__(self):
-        pass
+    row_index: int
+    column_index: int
+    items: List[Cell]
+    
+    def __init__(self, row_index: int):
+        self.row_index = row_index
+        self.column_index = 0
+        self.items = list()
+    
+    def add(self, widget: tk.Widget, *args, **kwargs):  # --> Self
+        if self.items:
+            self.column_index += self.items[-1].column_span
+        
+        self.items.append(Cell(
+            widget,
+            self.column_index,
+            self.row_index,
+            *args,
+            **kwargs,
+        ))
+        
+        return self
+    
+    def column_span(self, span: int):  # --> Self
+        if self.items:
+            self.items[-1].column_span = span
+        
+        return self
 
 
 class Grid:
+    rows: List[Row]
+    row_index: int
+    
     def __init__(self, parent, **kwargs):
         self.parent = parent
+        self.rows = []
+        self.row_index = 0
+        self.kwargs = kwargs
+    
+    def new_row(self) -> Row:
+        row = Row(self.row_index)
+        self.rows.append(row)
+        self.row_index += 1
+        return row
+    
+    def columnconfigure(self, i, *args, **kwargs):
+        self.parent.grid_columnconfigure(i, *args, **kwargs)
+
+    def rowconfigure(self, i, *args, **kwargs):
+        self.parent.grid_rowconfigure(i, *args, **kwargs)
+    
+    def build(self):
+        for row in self.rows:
+            for item in row.items:
+                kwargs = self.kwargs
+                kwargs.update(item.kwargs)
+                item.widget.grid(
+                    column=item.column_index,
+                    row=item.row_index,
+                    columnspan=item.column_span,
+                    rowspan=item.row_span,
+                    **kwargs,
+                )
 
 
 class DefaultRootWrapper:  # pragma: no cover
@@ -36,6 +100,8 @@ def grid_manager(parent, **kwargs):
     old_root = default_root_wrapper.default_root
     default_root_wrapper.default_root = parent
     try:
-        yield Grid(parent, **kwargs)
+        grid = Grid(parent, **kwargs)
+        yield grid
     finally:
+        grid.build()
         default_root_wrapper.default_root = old_root
